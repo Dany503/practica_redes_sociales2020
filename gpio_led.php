371e229589eade2799345ef6de1b85f35db5da2b
Daniel Gutiérrez <d.gutierrez.reina@gmail.com>
2020-11-10 12:39:56 +0100

    Add files via upload

---
+++ gpio_led.php
@@ -0,0 +1,7 @@
+<html>
+	<?php
+	exec("sudo python3 led_on.py");
+	echo "led encendido";
+	?>
+</html>
+
---
+++ led_off.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# importamos el modulo de GPIO
+import RPi.GPIO as GPIO
+
+
+
+GPIO.setmode(GPIO.BOARD) # definimos la numeracion 
+GPIO.setup(7, GPIO.OUT) # definimos el pin como salida
+GPIO.output(7, GPIO.LOW) # ponemos a 0 el pin
+#GPIO.output(7, GPIO.HIGH) # ponemos a 0 el pin
